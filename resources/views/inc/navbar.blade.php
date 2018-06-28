56fc22965db38240e3ee2b2fd000e34c61f3913a
kon <kon@localhost.localdomain>
2018-06-28 01:15:04 -0700

    fixed mobile navbar

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,70 +1,57 @@
 <nav class="navbar navbar-tiny">
-  {{--  <img src="/pictures/moki_logo_white.png" alt="test" srcset="">  --}}
   <a href="/"><img src="{{asset('/pictures/moki_logo_white.png')}}" alt="" srcset=""></a>
 </nav>
 
-<nav class="navbar navbar-expand-md navbar-dark bg-dark navbar-laravel">
-  <div class="container-fluid">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-        @guest
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
+    @guest
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
             <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
             <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
-          </ul>
-        </div>
-        @else
+        </ul>
+    @else
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                  <a class="nav-link" href="/"><i class="fas fa-home"></i> Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/tracking"><i class="fas fa-map-marker"></i> Tracking</a>
-                  </li>
-                {{--  <li class="nav-item">
-                  <a class="nav-link" href="/about">About</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/services">Services</a>
-                </li>  --}}
-                <li class="nav-item">
-                  <a class="nav-link" href="/devices"><i class="fas fa-box"></i> Devices</a>
-                </li>
-              </ul>
-        @endguest
-
-
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="/"><i class="fas fa-home"></i> Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/tracking"><i class="fas fa-map-marker"></i> Tracking</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/devices"><i class="fas fa-box"></i> Devices</a>
+            </li>
+        </ul>
+    @endguest
 
             <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
+        <ul class="navbar-nav ml-auto">
                 <!-- Authentication Links -->
-                @guest
-                    
-                @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
+        @guest
+        @else
+            <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    {{ Auth::user()->name }} <span class="caret"></span>
+                </a>
 
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="/dashboard">Settings</a>
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                              document.getElementById('logout-form').submit();">
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="/dashboard">Settings</a>
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
-                            </a>
+                    </a>
 
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
-        </div>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+        @endguest
+        </ul>
     </div>
 </nav>
