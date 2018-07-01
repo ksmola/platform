d7cc0d3eaf7419bf09a74053041f82a046c16fac
konstantin <kon@localhost.localdomain>
2018-07-01 13:28:30 -0700

    updated user reg

--- app/User.php
+++ app/User.php
@@ -24,7 +24,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'id', 'password', 'remember_token',
+        'password', 'remember_token',
     ];
 
     public function devices() {
